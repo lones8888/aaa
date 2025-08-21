490cdd8245de7267b213ab6d4baa9a55290bf901
lones8888 <104973334+lones8888@users.noreply.github.com>
2025-08-21 23:03:26 +0300

    Update voyage.py

--- voyage.py
+++ voyage.py
@@ -1,34 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+from playwright.sync_api import sync_playwright
 import requests
 
 BOT_TOKEN = "8064693875:AAFEHpkHFMTnqPno2gZB19FHAbyCMVtmWGQ"
 CHAT_ID = "-1002950043362"
 
-url = "https://www.etstur.com/Voyage-Sorgun"
+def send_photo(photo_path):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto"
+    with open(photo_path, "rb") as f:
+        requests.post(url, data={"chat_id": CHAT_ID}, files={"photo": f})
 
-# Chrome options
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto("https://www.etstur.com/Voyage-Sorgun")
+    page.screenshot(path="voyage.png", full_page=True)
+    browser.close()
 
-driver = webdriver.Chrome(options=options)
-driver.get(url)
-time.sleep(5)  # sayfanın yüklenmesini bekle
-
-# ekran görüntüsü kaydet
-screenshot_path = "screenshot.png"
-driver.save_screenshot(screenshot_path)
-driver.quit()
-
-# Telegram'a gönder
-with open(screenshot_path, "rb") as img:
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto",
-        data={"chat_id": CHAT_ID, "caption": "Voyage Sorgun Güncel Fiyatlar"},
-        files={"photo": img}
-    )
-
-
+send_photo("voyage.png")
