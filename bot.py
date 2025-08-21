fc974b69f6cb3eaadf0a34b0ee9fdce62c0df500
lones8888 <104973334+lones8888@users.noreply.github.com>
2025-08-21 22:24:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -8,16 +9,17 @@ CHAT_ID = "-1002950043362"
 URL = "https://www.etstur.com/Voyage-Sorgun?check_in=2026-09-06&check_out=2026-09-11&adult_1=2&child_1=0"
 
 def take_screenshot(url, filename="screenshot.png"):
+    chromedriver_autoinstaller.install()  # ChromeDriver otomatik indirilecek
+
     options = Options()
-    options.add_argument("--headless")  
-    options.add_argument("--disable-gpu")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1280,2000")
 
     driver = webdriver.Chrome(options=options)
     driver.get(url)
-    time.sleep(8)  # sayfanın tamamen yüklenmesi için biraz bekle
+    time.sleep(8)  # sayfanın tam yüklenmesi için bekle
     driver.save_screenshot(filename)
     driver.quit()
     return filename
